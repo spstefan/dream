9b392403a67f39ca9424b2cc9012578338738edb
spstefan <stefansp@uio.no>
2024-07-06 00:34:15 +0200

    setup server

---
+++ server.js
@@ -0,0 +1,32 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+import {OpenAI} from 'openai';
+
+const openai = new OpenAI({
+    apiKey: process.env.OPENAI,
+});
+
+import express from 'express';
+import cors from 'cors';
+
+const app = express ();
+app.use(cors()); 
+app.use(express.json());
+
+app.post('/dream', async(req, res) => {
+
+    const prompt = req.body.prompt;
+
+    const aiResponse = await openai.images.generate({
+        prompt,
+        n: 1,
+        size: '1024x1024'
+    });
+
+    const image = aiResponse.data[0].url;
+    res.send({image});
+});
+
+app.listen(8080, () => console.log('make art on http://localhost:8080/dream'));
+
