3d9d0c99dc4c184ab9da26cfc9156c7083de1d99
spstefan <stefansp@uio.no>
2024-07-27 23:10:14 +0200

    error handling

--- server.js
+++ server.js
@@ -16,16 +16,22 @@ app.use(express.json());
 
 app.post('/dream', async(req, res) => {
 
-    const prompt = req.body.prompt;
+    try {
+        const prompt = req.body.prompt;
 
-    const aiResponse = await openai.images.generate({
-        prompt,
-        n: 1,
-        size: '1024x1024'
-    });
+        const aiResponse = await openai.images.generate({
+            prompt,
+            n: 1,
+            size: '1024x1024'
+        });
 
-    const image = aiResponse.data[0].url;
-    res.send({image});
+        const image = aiResponse.data[0].url;
+        console.log('Generated image URL:', image); // Log the generated image URL
+        res.send({image});
+    } catch (error) {
+        console.error(error)
+        res.status(500).send(error?.response.data.error.message || 'Something went wrong');
+    }
 });
 
 app.listen(8080, () => console.log('make art on http://localhost:8080/dream'));
